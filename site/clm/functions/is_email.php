837f03c91ff62ba1892ee3a8a3f4d524fd27ebcc
kbaerthel <kbaerthel@t-online.de>
2015-08-23 19:04:19 +0200

    # Config: Email-Adressen mit Umlauten erlauben

--- site/clm/functions/is_email.php
+++ site/clm/functions/is_email.php
@@ -1,7 +1,15 @@
 <?php
-// http://snipplr.com/view/5226/php--isvalidemail/
 function clm_function_is_email($email)
 {
-    return (filter_var($email, FILTER_VALIDATE_EMAIL) == $email ? true : false);
+// Include the class
+	$path = clm_core::$path . DS . "includes" . DS . "idna_convert.class" . '.php';
+	require_once ($path);
+	$parts = explode('@', $email);
+	if (count($parts) != 2) return false;
+// Instantiate it (depending on the version you are using) with
+	$IDN = new idna_convert();
+// Encode it to its punycode presentation
+	$parts1 = $IDN->encode($parts[1]); 
+    return (filter_var($parts[0].'@'.$parts1, FILTER_VALIDATE_EMAIL) !== false ? true : false);
 }
 ?>
