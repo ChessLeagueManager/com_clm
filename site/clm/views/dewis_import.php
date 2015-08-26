ad0e7cbe1fb9b91af54081e1f8168ad36cee82cd
kbaerthel <kbaerthel@t-online.de>
2015-08-26 23:58:05 +0200

    # Admin-View Datenbank: Text-Korrektur

--- site/clm/views/dewis_import.php
+++ site/clm/views/dewis_import.php
@@ -10,7 +10,7 @@ function clm_view_dewis_import() {
 <?php $fix = clm_core::$api->view_units_form("",clm_core::$db->config()->lv); echo $fix[2]; // array dereferencing fix php 5.3 ?>
 <button type="button" class="clm_button" onclick="clm_dewis_import_club(this)" title="<?php echo $lang->clubUpdateText; ?>"><?php echo $lang->clubUpdate; ?></button>
 <button type="button" class="clm_button" onclick="clm_dewis_import_player(this,0)" title="<?php echo $lang->normalUpdateText; ?>"><?php echo $lang->normalUpdate; ?></button>
-<button type="button" class="clm_button" onclick="clm_dewis_import_player(this,1)" title="<?php echo $lang->normalUpdateText; ?>"><?php echo $lang->specialUpdate; ?></button>
+<button type="button" class="clm_button" onclick="clm_dewis_import_player(this,1)" title="<?php echo $lang->specialUpdateText; ?>"><?php echo $lang->specialUpdate; ?></button>
 </div>
 
 <div class="clm_view_dewis_import_update" style="display:none;">
