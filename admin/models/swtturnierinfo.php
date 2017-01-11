a51cf0c78583fe5e258499939cf42850a6276d94
Klaus Bärthel <kbaerthel@t-online.de>
2017-01-11 13:38:52 +0100

    # SWT-Import Einzel: Partiewertung

--- admin/models/swtturnierinfo.php
+++ admin/models/swtturnierinfo.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @ Chess League Manager (CLM) Component 
- * @Copyright (C) 2008-2016 CLM Team.  All rights reserved
+ * @Copyright (C) 2008-2017 CLM Team.  All rights reserved
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link http://www.chessleaguemanager.de
  * @author Thomas Schwietert
@@ -250,7 +250,7 @@ class CLMModelSWTTurnierInfo extends JModelLegacy {
 			$this->_turnier->set('optionTiebreakersFideCorrect', CLMSWT::readBool($swt,675));
 			
 			//Partiewertung
-			$pwertung = CLMSWT::readInt($swt,582,1);
+			$pwertung = CLMSWT::readInt($swt,1336,1);
 			if ($pwertung > 3) $pwertung = 0;
 			if ($pwertung == 0) {
 				$this->_turnier->set('sieg'				, 1);
