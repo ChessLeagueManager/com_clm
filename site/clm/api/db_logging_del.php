e552bc9ae4e933de99ec72a54ba5a14bfb9af9a9
Klaus Bärthel <kbaerthel@t-online.de>
2017-08-09 21:10:03 +0200

    ^ Logging: alle Einträge löschen -> alle älter als 450 Tage

--- site/clm/api/db_logging_del.php
+++ site/clm/api/db_logging_del.php
@@ -2,6 +2,7 @@
 function clm_api_db_logging_del($id) {
 	if($id==null) {
 		$sql =  "DELETE FROM #__clm_logging";
+		$sql .= " WHERE timestamp < ".(time()-(450 * 24 * 60 * 60));
 		clm_core::$db->query($sql);
 	}
 	if(is_numeric($id)) {
