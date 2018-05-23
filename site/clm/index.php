b479a5e5d80e631dfc34215ae25c00b3d0df4048
Klaus Bärthel <kbaerthel@t-online.de>
2018-05-23 23:32:59 +0200

    Version 3.6.0

--- site/clm/index.php
+++ site/clm/index.php
@@ -8,7 +8,7 @@
 // CLM als aktiv markieren, Major.Minor.Patch:Datenbankversion
 // WICHTIG: Gibt es eine neue Datenbankversion,
 // müssen die Änderungen auch in der install.sql eingebracht werden.
-define("clm", "3.5.2:29");
+define("clm", "3.6.0:29");
 if (!defined("DS")) {
 	define('DS', DIRECTORY_SEPARATOR);
 }
