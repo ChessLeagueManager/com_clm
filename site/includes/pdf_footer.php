03011805f080f0b88deb82ae4e38c0e356cea03f
Klaus Bärthel <kbaerthel@t-online.de>
2019-01-09 23:52:56 +0100

    ^ Copyright 2008-2019

--- site/includes/pdf_footer.php
+++ site/includes/pdf_footer.php
@@ -9,7 +9,7 @@
 	$this->SetFont('Arial','I',8);
 	//Seitenzahl
 	$this->Cell(0,5,JText::_( 'PDF_PAGE' ).$this->PageNo().'/{nb}',0,1,'C');
-	$this->Cell(10,5,'(c)CLM 2008-2018',0,0,'L');
+	$this->Cell(10,5,'(c)CLM 2008-2019',0,0,'L');
 //	$this->Cell(0,5,'Erstellt mit CLM - ChessLeagueManager [ http://www.chessleaguemanager.de ]',0,0,'C');
 	$this->Cell(0,5,JText::_( 'GENERATED_BY_CLM' ),0,0,'C');
 ?>
