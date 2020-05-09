b639c827794140f4e7fd2dddbd4c9bba831a807e
Klaus Bärthel <kbaerthel@t-online.de>
2020-05-09 23:22:30 +0200

    ^ Code-Anpassung php 7.4

--- site/clm/views/notification.php
+++ site/clm/views/notification.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @ Chess League Manager (CLM) Component 
+ * @Copyright (C) 2008-2020 CLM Team.  All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.chessleaguemanager.de
+**/
 function clm_view_notification($string, $intern = true) {
 	$lang = clm_core::$lang->notification;
 	if ($intern) {
@@ -7,25 +13,25 @@ function clm_view_notification($string, $intern = true) {
 	if (!$lang->exist($string)) {
 		$string = "e_noNotification";
 	}
-	if ($string{0} == "e") {
+	if ($string[0] == "e") {
 		if ($intern) {
 			echo '<div class="error"><span>' . $lang->$string . '</span></div>';
 		} else {
 			return array($lang->$string, 'error');
 		}
-	} else if ($string{0} == "w") {
+	} else if ($string[0] == "w") {
 		if ($intern) {
 			echo '<div class="warning"><span>' . $lang->$string . '</span></div>';
 		} else {
 			return array($lang->$string, 'warning');
 		}
-	} else if ($string{0} == "m") {
+	} else if ($string[0] == "m") {
 		if ($intern) {
 			echo '<div class="success"><span>' . $lang->$string . '</span></div>';
 		} else {
 			return array($lang->$string, 'message');
 		}
-	} else if ($string{0} == "n") {
+	} else if ($string[0] == "n") {
 		if ($intern) {
 			echo '<div class="notice"><span>' . $lang->$string . '</span></div>';
 		} else {
