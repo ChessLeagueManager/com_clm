00ad695f1c440b636b69a911d60ccb03ec71e702
Klaus Bärthel <kbaerthel@t-online.de>
2020-05-11 16:52:59 +0200

    # Verwendung Parameter CLM-Template-Div im Frontend

--- site/includes/css_path.php
+++ site/includes/css_path.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @ Chess League Manager (CLM) Component 
+ * @Copyright (C) 2008-2020 CLM Team.  All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.chessleaguemanager.de
+*/
 // Pfade zum Style Sheet und Stylesheet laden !
 // Es gab ein paar Probleme mit dem Joomla Dateiseperator die diese Massnahme noetig machen
 
@@ -39,7 +45,8 @@
 	if ($template) {
 
 	$document = JFactory::getDocument();
-	$document->addStyleSheet('components/com_clm/includes/clm_content.css', 'text/css');
+	if ($config->div == 1) {
+		$document->addStyleSheet('components/com_clm/includes/clm_content.css', 'text/css');
 ?>
 <style type="text/css">
 
@@ -83,5 +90,54 @@
 #clm .clm .rang_ab_evtl {background-color: <?php echo $clm_rang_ab_evtl; ?> !important;}
 
 </style>
+<?php
+	} else {
+		$document->addStyleSheet('components/com_clm/includes/clm_content_0.css', 'text/css');
+
+?>
+<style type="text/css">
+
+.clm .pgn a, .clm  a.pgn { color:<?php echo $fe_pgn_show; ?> !important; }
+
+<?php if ($clm_lesehilfe =="1") { ?>.clm tr.zeile1:hover td, .clm tr.zeile2:hover td, .clm tr.zeile1_auf:hover td, .clm tr.zeile2_auf:hover td {background-color: #FFFFBB !important;}  /*-- Lesehilfe --*/
+<?php } ?>
+
+.clm table th, .clm table td { padding-left: <?php echo $clm_cellin_left; ?>; padding-top: <?php echo $clm_cellin_top; ?>; padding-right: <?php echo $clm_cellin_right; ?>; padding-bottom: <?php echo $clm_cellin_bottom; ?>; border: <?php echo $clm_border; ?>; }
+#spieler div.title, #spieler div.spielerverlauf  { border-bottom: none !important; border: <?php echo $clm_border; ?>;}
+.right .selectteam  { border: <?php echo $clm_border; ?>;}
+
+.clm .zeile1, .clm .zeile1_dg2 { background-color: <?php echo $clm_zeile1; ?>;} 
+.clm .zeile2, .clm .zeile2_dg2 { background-color: <?php echo $clm_zeile2; ?>;} 
+
+#mannschaft td.punkte, #mannschaft td.spiele, #mannschaft td.prozent, #turnier_tabelle td.pkt, #turnier_tabelle td.sobe, #turnier_tabelle td.bhlz, #turnier_tabelle td.busu, #rangliste td.mp, #rangliste td.bp, td.fw_col {background-color: <?php echo $clm_re_col; ?> !important;} 
+
+.clm table th, .clm table th div{ background-color: <?php echo $clm_tableth; ?>; color: <?php echo $clm_tableth_s1; ?> !important;}
+.clm table .anfang, .clm table .ende, .clm .clmbox, #spieler table.spielerverlauf th, #spieler div.spielerverlauf, #spieler .title, .clm .clm-navigator ul li, .clm .clm-navigator ul li ul{ background-color: <?php echo $clm_subth; ?>; color: <?php echo $clm_tableth_s2; ?> !important;}
+
+#vereinsliste th.col_1, #vereinsliste th.col_2,#vereinsliste th.col_3,#vereinsliste th.col_4,#vereinsliste th.col_5,#vereinsliste th.col_6,#vereinsliste th.col_7,#vereinsliste th.col_8, #vereinsliste th.col{background-color: <?php echo $clm_subth; ?>; color: <?php echo $clm_tableth_s2; ?> !important;}
+
+#wrong, .wrong {background:<?php echo $clm_wrong1; ?>; border:<?php echo $clm_wrong2; ?>;}
+
+#mannschaft .nr { width: <?php echo $clm_msch_nr; ?>; } 
+#mannschaft .dwz { width: <?php echo $clm_msch_dwz; ?>; } 
+#mannschaft .rnd { width: <?php echo $clm_msch_rnd; ?>; }
+#mannschaft .punkte { width: <?php echo $clm_msch_punkte; ?>; }
+#mannschaft .spiele { width: <?php echo $clm_msch_spiele; ?>; }
+#mannschaft .prozent{ width: <?php echo $clm_msch_prozent; ?>; } 
+
+/* Lesehilfe AUS bei Einzelturniermodus */
+#tableoverflow tr.zeile1:hover td, #tableoverflow tr.zeile2:hover td, #tableoverflow tr.zeile1_auf:hover td, #tableoverflow tr.zeile2_auf:hover td {background: none !important;}
+#tableoverflow tr.zeile1:hover td.sobe, #tableoverflow tr.zeile2:hover td.sobe, #tableoverflow tr.zeile1:hover td.fw_col, #tableoverflow tr.zeile2:hover td.fw_col, #tableoverflow tr.zeile1:hover td.pkt, #tableoverflow tr.zeile2:hover td.pkt,#tableoverflow tr.zeile1:hover td.busu, #tableoverflow tr.zeile2:hover td.busu{background-color: #FFFFCC !important;} 
+#tableoverflow tr.zeile1:hover td.trenner, #tableoverflow tr.zeile2:hover td.trenner {background-color: #E8E8E8 !important;}
+/** Auf- und Abstieg **/
+
+.clm .rang_auf {background-color: <?php echo $clm_rang_auf; ?> !important;} 
+.clm .rang_auf_evtl {background-color: <?php echo $clm_rang_auf_evtl; ?> !important;} 
+.clm .rang_ab {background-color: <?php echo $clm_rang_ab; ?> !important;} 
+.clm .rang_ab_evtl {background-color: <?php echo $clm_rang_ab_evtl; ?> !important;}
+
+</style>
+
+<?php } ?>
 
 <?php } ?>
