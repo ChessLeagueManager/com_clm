e6c1ff39cebf4bea51f484a5f089dff5ab537e7f
Klaus Bärthel <kbaerthel@t-online.de>
2020-06-02 23:48:03 +0200

    # DeWIS-Dateiausgabe: mehrere Durchgänge bei Einzelturnier

--- admin/models/auswertung.php
+++ admin/models/auswertung.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * @ Chess League Manager (CLM) Component 
- * @Copyright (C) 2008-2017 CLM Team.  All rights reserved
+ * @Copyright (C) 2008-2020 CLM Team.  All rights reserved
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * @link http://www.fishpoke.de
+ * @link http://www.chessleaguemanager.de
  * @author Thomas Schwietert
  * @email fishpoke@fishpoke.de
  * @author Andreas Dorn
@@ -404,7 +404,7 @@ function datei() {
 	} else {
 		$sql = " SELECT * FROM #__clm_turniere_rnd_termine "
 			." WHERE sid = '$sid' AND turnier = '".$liga_name[0]->id."'"
-			." ORDER BY nr ASC "
+			." ORDER BY dg ASC, nr ASC "
 			;
 	}
 	$db->setQuery($sql);
@@ -419,8 +419,13 @@ function datei() {
 	/////////////////////////////////
 		
 	foreach($runden as $rdata){
+		if(!$et){
+			$temp_nr = $rdata->nr;
+		} else {
+			$temp_nr = $rdata->nr + (($rdata->dg - 1) * $liga_name[0]->runden);
+		}
 		$xml .= '<appointment>'
-			.'<round>'.$rdata->nr.'</round>'
+			.'<round>'.$temp_nr.'</round>'
 			.'<matchday>'.$rdata->datum.'</matchday>'
 			.'<matchtime>'.$rdata->startzeit.'</matchtime>'
 			.'</appointment>'
