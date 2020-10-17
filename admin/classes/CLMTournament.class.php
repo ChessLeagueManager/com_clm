0dd6d7c590146f8f6673328db5777c4776a9a565
Klaus Bärthel <kbaerthel@t-online.de>
2020-10-17 18:38:22 +0200

    # Einzelturniere: Feinwertung Buchholzsumme

--- admin/classes/CLMTournament.class.php
+++ admin/classes/CLMTournament.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @ Chess League Manager (CLM) Component 
- * @Copyright (C) 2008-2019 CLM Team  All rights reserved
+ * @Copyright (C) 2008-2020 CLM Team  All rights reserved
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link http://www.chessleaguemanager.de
  * @author Thomas Schwietert
@@ -557,7 +557,8 @@ class CLMTournament {
 							$array_PlayerBuSum1StMin[$value->tln_nr] = $array_PlayerBuch1St[$value->gegner];
 			}
 			for ($s=1; $s<= $this->data->teil; $s++) { // alle Startnummern durchgehen
-				$array_PlayerBuSum1St[$s] = $array_PlayerBuSum1St[$s] - $array_PlayerBuSum1StMin[$s];
+				if ($array_PlayerBuSum1StMin[$s] < 999)
+					$array_PlayerBuSum1St[$s] = $array_PlayerBuSum1St[$s] - $array_PlayerBuSum1StMin[$s];
 			}
 		}
 	
