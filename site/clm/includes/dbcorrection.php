d79c42879a0c5e74d58ce831f4022cc8df540fa7
Klaus Bärthel <kbaerthel@t-online.de>
2020-12-08 21:21:29 +0100

    + DB: Routine zur Korrektur leerer Felder um Feldtyp date erweitert

--- site/clm/includes/dbcorrection.php
+++ site/clm/includes/dbcorrection.php
@@ -12,7 +12,7 @@ defined('clm') or die('Restricted access');
 //  
 // Start dieser Routine mit z.B. http://localhost/hallep/administrator/index.php?option=com_clm&view=forceDBCorrection
 // 
-// Diese Routine behandelt DB-Felder vom Typ datetime und ist nötig bei MySQL 5.7.26
+// Diese Routine behandelt DB-Felder der Typen datetime und date und ist nötig z.B. bei MySQL 5.7.26
 // 
 // Außerkraftsetzen der Feldprüfung 
 $query = "SET SESSION SQL_MODE='ALLOW_INVALID_DATES'";
@@ -358,5 +358,179 @@ $total = clm_core::$db->affected_rows();
 if ($total != 0)
 	echo "<br>Table  <b>clm_turniere_tlnr</b>  Field  <b>checked_out_time</b>  has been corrected , $total rows affected.<br>";
 
+//-------------------------------------------------------------------------------------------------------------------------------------------
+// Felder vom Typ date
+$total = -1;
+$query = "UPDATE #__clm_categories"
+		." SET dateStart = '1970-01-01' "
+		." WHERE dateStart = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_categories</b>  Field  <b>dateStart</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_categories"
+		." SET dateEnd = '1970-01-01' "
+		." WHERE dateEnd = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_categories</b>  Field  <b>dateEnd</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_rangliste_name"
+		." SET Meldeschluss = '1970-01-01' "
+		." WHERE Meldeschluss = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_rangliste_name</b>  Field  <b>Meldeschluss</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_rnd_man"
+		." SET pdate = '1970-01-01' "
+		." WHERE pdate = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_rnd_man</b>  Field  <b>pdate</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_runden_termine"
+		." SET datum = '1970-01-01' "
+		." WHERE datum = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_runden_termine</b>  Field  <b>datum</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_runden_termine"
+		." SET deadlineday = '1970-01-01' "
+		." WHERE deadlineday = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_runden_termine</b>  Field  <b>deadlineday</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_runden_termine"
+		." SET enddatum = '1970-01-01' "
+		." WHERE enddatum = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_runden_termine</b>  Field  <b>enddatum</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_saison"
+		." SET datum = '1970-01-01' "
+		." WHERE datum = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_saison</b>  Field  <b>datum</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_swt_turniere"
+		." SET dateStart = '1970-01-01' "
+		." WHERE dateStart = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_swt_turniere</b>  Field  <b>dateStart</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_swt_turniere"
+		." SET dateEnd = '1970-01-01' "
+		." WHERE dateEnd = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_swt_turniere</b>  Field  <b>dateEnd</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_swt_turniere_rnd_termine"
+		." SET datum = '1970-01-01' "
+		." WHERE datum = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_swt_turniere_rnd_termine</b>  Field  <b>datum</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_termine"
+		." SET startdate = '1970-01-01' "
+		." WHERE startdate = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_termine</b>  Field  <b>startdate</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_termine"
+		." SET enddate = '1970-01-01' "
+		." WHERE enddate = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_termine</b>  Field  <b>enddate</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_turniere"
+		." SET dateStart = '1970-01-01' "
+		." WHERE dateStart = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_turniere</b>  Field  <b>dateStart</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_turniere"
+		." SET dateEnd = '1970-01-01' "
+		." WHERE dateEnd = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_turniere</b>  Field  <b>dateEnd</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_turniere"
+		." SET dateRegistration = '1970-01-01' "
+		." WHERE dateRegistration = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_turniere</b>  Field  <b>dateRegistration</b>  has been corrected , $total rows affected.<br>";
+
+$total = -1;
+$query = "UPDATE #__clm_turniere_rnd_termine"
+		." SET datum = '1970-01-01' "
+		." WHERE datum = '0000-00-00' "
+		;
+clm_core::$db->query($query);
+$total = clm_core::$db->affected_rows();		
+if ($total != 0)
+	echo "<br>Table  <b>clm_turniere_rnd_termine</b>  Field  <b>datum</b>  has been corrected , $total rows affected.<br>";
+
+	
+		
 	
 ?>
