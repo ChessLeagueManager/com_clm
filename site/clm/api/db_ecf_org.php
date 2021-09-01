f84dac597c984e3619b235589260404ebda9f63a
Klaus Bärthel <kbaerthel@t-online.de>
2021-09-01 18:32:36 +0200

    ^ Code-Anpassung Joomla 4: Umsetzungsroutine auf engl. Anwendung

--- site/clm/api/db_ecf_org.php
+++ site/clm/api/db_ecf_org.php
@@ -1,20 +1,35 @@
 <?php
+/**
+ * @ Chess League Manager (CLM) Component 
+ * @Copyright (C) 2008-2021 CLM Team.  All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.chessleaguemanager.de
+*/
 function clm_api_db_ecf_org($orgs = array()) {
 	@set_time_limit(0); // hope
 	if (!is_array($orgs) || count($orgs) == 0) {
 		return array(true, "w_noOrgToUpdate");
 	}
 	// Umbau der DB von deutscher Version auf englische Version, einmalige Aktion
-	$db = JFactory::getDBO();
-	$keys = $db->getTableKeys($db->getPrefix()."clm_dwz_spieler");
+//	$db = JFactory::getDBO();
+//	$keys = $db->getTableKeys($db->getPrefix()."clm_dwz_spieler");
+	$query = 'SHOW KEYS FROM #__clm_dwz_spieler';
+	$keys = clm_core::$db->loadObjectList($query);
 	$ks_mglnr = 0;
 	$ks_pkz = 0;
 	foreach($keys as $key) {
 		if ($key->Key_name == "sid_zps_mglnr" AND $key->Non_unique == "0") $ks_mglnr = 1;
 		if ($key->Key_name == "sid_zps_pkz") $ks_pkz = 1;
 	}
-	$columns = $db->getTableColumns($db->getPrefix()."clm_dwz_verbaende");
-	if (!isset($columns["Allocation"])) {
+//	$columns = $db->getTableColumns($db->getPrefix()."clm_dwz_verbaende");
+	$query = 'SHOW FULL COLUMNS FROM #__clm_dwz_verbaende';
+	$columns = clm_core::$db->loadObjectList($query);
+	$s_allocation = false;
+	foreach($columns as $column) {
+		if ($column->Field == "Allocation" ) $s_allocation = true;
+	}
+//	if (!isset($columns["Allocation"])) {
+	if ($s_allocation == false) {
 		//Leeren Verbaende, da standardmäßig mit DSB-Struktur gefüllt
 		$sql = "TRUNCATE #__clm_dwz_verbaende";
 		clm_core::$db->query($sql);	
