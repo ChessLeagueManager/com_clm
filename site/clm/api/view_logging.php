22cc9054909b0b188076bb865a38d534635212b1
Klaus Bärthel <kbaerthel@t-online.de>
2023-08-22 20:37:59 +0200

    # Admin-View Logging: Nachbesserung Berechtigungen

--- site/clm/api/view_logging.php
+++ site/clm/api/view_logging.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @ Chess League Manager (CLM) Component 
+ * @Copyright (C) 2008-2023 CLM Team.  All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.chessleaguemanager.de
+*/
 function clm_api_view_logging() {
 	$lang = clm_core::$lang->logging;
 	$table = new clm_class_table("logging","db_logging",8,7,false);
@@ -8,9 +14,11 @@ function clm_api_view_logging() {
 	clm_core::$load->load_css("modal");
 	//Andere Position?
 	
-	$table->add_button($lang->new,$lang->new_title,clm_core::$url.clm_core::$load->gen_url(array("view"=>"view_logging_new")),"clm_table_form clm_button clm_button_new clm_button_big clm_button_big_new");
-	$table->add_button($lang->del,$lang->del_title,json_encode(array("db_logging_del",array(""))),"clm_table_event_special clm_table_confirm clm_button clm_button_cancel clm_button_big clm_button_big_cancel");
-		
+	$clmAccess = clm_core::$access;
+	if($clmAccess->access('BE_logfile_delete')) {
+		$table->add_button($lang->new,$lang->new_title,clm_core::$url.clm_core::$load->gen_url(array("view"=>"view_logging_new")),"clm_table_form clm_button clm_button_new clm_button_big clm_button_big_new");
+		$table->add_button($lang->del,$lang->del_title,json_encode(array("db_logging_del",array(""))),"clm_table_event_special clm_table_confirm clm_button clm_button_cancel clm_button_big clm_button_big_cancel");
+	}
 	$table->add_filter_field("type",array("" => $lang->type,0 => $lang->type0,1 => $lang->type1,2 => $lang->type2,3 => $lang->type3,4 => $lang->type4));
 	$fix= clm_core::$load->load_view("table", $table->result());
 	return array(true,"m_tableSuccess","<div class='clm'>".$fix[1]."</div>");
