860cee3a42aa52f25914b35fdac906ad1fef867a
Klaus Bärthel <kbaerthel@t-online.de>
2024-04-08 21:23:43 +0200

    # pgn-Eingabe: Problem mit "" bei manueller Zuordnung

--- admin/models/pgndata.php
+++ admin/models/pgndata.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @ Chess League Manager (CLM) Component 
- * @Copyright (C) 2008-2021 CLM Team.  All rights reserved
+ * @Copyright (C) 2008-2024 CLM Team.  All rights reserved
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link http://www.chessleaguemanager.de
  * @author Thomas Schwietert
@@ -123,7 +123,8 @@ class CLMModelPGNdata extends JModelLegacy {
 			if ($tkz == 't') 
 				$query .= ' , paar = '.clm_escape(clm_core::$load->request_int('paar'.$p));
 			$query .= ' , brett = '.clm_escape(clm_core::$load->request_int('brett'.$p))
-				." , text = '".clm_escape(clm_core::$load->request_string('text'.$p))."'"
+//				." , text = '".clm_escape(clm_core::$load->request_string('text'.$p))."'"
+				." , text = '".clm_escape($_POST['text'.$p])."'"
 				." , error = ''"
 				.' WHERE id = '.clm_escape(clm_core::$load->request_int('pgnnr'.$p));
 //echo "<br>in_query: "; var_dump($query); //die(); 
