1fe60b113eeeba21ad5008a4acb7fcc242e5fcd8
Klaus Bärthel <kbaerthel@t-online.de>
2024-05-12 22:29:49 +0200

    Version 4.1.5

--- site/clm/index.php
+++ site/clm/index.php
@@ -8,7 +8,7 @@
 // CLM als aktiv markieren, Major.Minor.Patch:Datenbankversion
 // WICHTIG: Gibt es eine neue Datenbankversion,
 // müssen die Änderungen auch in der install.sql eingebracht werden.
-define("clm", "4.1.4b:57");
+define("clm", "4.1.5:57");
 if (!defined("DS")) {
 	define('DS', DIRECTORY_SEPARATOR);
 }
