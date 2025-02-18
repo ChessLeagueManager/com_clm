ce804201e4ffd3f9ab30ecba6e0d6de27f03ee67
Klaus Bärthel <kbaerthel@t-online.de>
2025-02-18 16:44:23 +0100

    Version 4.2.3

--- site/clm/index.php
+++ site/clm/index.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @ Chess League Manager (CLM) Component 
- * @Copyright (C) 2008-2024 CLM Team.  All rights reserved
+ * @Copyright (C) 2008-2025 CLM Team.  All rights reserved
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link https://www.chessleaguemanager.de
 */
 // CLM als aktiv markieren, Major.Minor.Patch:Datenbankversion
 // WICHTIG: Gibt es eine neue Datenbankversion,
 // müssen die Änderungen auch in der install.sql eingebracht werden.
-define("clm", "4.2.2:67");
+define("clm", "4.2.3:67");
 if (!defined("DS")) {
 	define('DS', DIRECTORY_SEPARATOR);
 }
