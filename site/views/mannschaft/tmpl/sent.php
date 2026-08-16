0a10c80f60f23248a2b9dbe07a7c1f13bf78bd07
root <root@clmdev-1.frednet>
2026-08-16 16:10:09 +0200

    # utf2html von Hand, statt von iconv

--- site/views/mannschaft/tmpl/sent.php
+++ site/views/mannschaft/tmpl/sent.php
@@ -37,11 +37,45 @@ $user =Factory::getUser();
 $link = URI::base(true) .'index.php?option=com_clm&view=mannschaft&saison='. $sid . '&liga=' . $liga . '&tlnr=' . $tlnr;
 
 function utf2html($str) {
-	$ret = iconv("UTF-8", "ISO-8859-1", $str);
-	if ($ret === false) {
-		return $str;
-	}
-	return $ret;
+	$str = str_replace('Ä', '&Auml;', $str);
+	$str = str_replace('Á', '&Aacute;', $str);
+	$str = str_replace('À', '&Agrave;', $str);
+	$str = str_replace('Â', '&Acirc;', $str);
+	$str = str_replace('Ç', '&Ccedil;', $str);
+	$str = str_replace('Ë', '&Euml;', $str);
+	$str = str_replace('É', '&Eacute;', $str);
+	$str = str_replace('È', '&Egrave;', $str);
+	$str = str_replace('Ê', '&Ecirc;', $str);
+	$str = str_replace('Ï', '&Iuml;', $str);
+	$str = str_replace('Ñ', '&Ntilde;', $str);
+	$str = str_replace('Ö', '&Ouml;', $str);
+	$str = str_replace('Ó', '&Oacute;', $str);
+	$str = str_replace('Ò', '&Ograve;', $str);
+	$str = str_replace('Ô', '&Ocirc;', $str);
+	$str = str_replace('Õ', '&Otilde;', $str);
+	$str = str_replace('Ü', '&Uuml;', $str);
+	$str = str_replace('Ÿ', '&Yuml;', $str);
+	$str = str_replace('ä', '&auml;', $str);
+	$str = str_replace('á', '&aacute;', $str);
+	$str = str_replace('à', '&agrave;', $str);
+	$str = str_replace('â', '&acirc;', $str);
+	$str = str_replace('ç', '&ccedil;', $str);
+	$str = str_replace('ë', '&euml;', $str);
+	$str = str_replace('é', '&eacute;', $str);
+	$str = str_replace('è', '&egrave;', $str);
+	$str = str_replace('ê', '&ecirc;', $str);
+	$str = str_replace('ï', '&iuml;', $str);
+	$str = str_replace('ñ', '&ntilde;', $str);
+	$str = str_replace('ö', '&ouml;', $str);
+	$str = str_replace('ó', '&oacute;', $str);
+	$str = str_replace('ò', '&ograve;', $str);
+	$str = str_replace('ô', '&ocirc;', $str);
+	$str = str_replace('õ', '&otilde;', $str);
+	$str = str_replace('ü', '&uuml;', $str);
+	$str = str_replace('ÿ', '&yuml;', $str);
+	$str = str_replace('ß', '&szlig;', $str);
+	$str = str_replace('´', '&rsquo;', $str);
+	return $str;
 }
 
 function hexdump($str) {
