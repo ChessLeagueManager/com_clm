c55386201b05806ef2e3f69be8091922d8bc7d21
kbaerthel <kbaerthel@t-online.de>
2026-09-04 00:02:37 +0200

    Version 5.1.1

--- site/clm/index.php
+++ site/clm/index.php
@@ -8,7 +8,7 @@
 // CLM als aktiv markieren, Major.Minor.Patch:Datenbankversion
 // WICHTIG: Gibt es eine neue Datenbankversion,
 // müssen die Änderungen auch in der install.sql eingebracht werden.
-define("clm", "5.1.0d:82");
+define("clm", "5.1.1:82");
 if (!defined("DS")) {
 	define('DS', DIRECTORY_SEPARATOR);
 }
